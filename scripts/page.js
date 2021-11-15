122232b580c7f8a0cec65f83cc4ff19ff80b1de2
Xilin Wang <stinsionwang@gmail.com>
2021-11-14 21:02:32 -0500

    xw added plan page

---
+++ scripts/page.js
@@ -0,0 +1,21 @@
+
+
+
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+  
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function displayForm() {
+    console.log("display");
+    document.getElementById("myForm").style.display = "block";
+}
+
+function showForm() {
+    console.log("testing");
+    document.getElementById("myForm").show();
+}
